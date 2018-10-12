e9d1ffe15a6d2217cc820231f93a04343140e755
Rafael Bastiansch <rafaelbastiansch@gmail.com>
2018-10-12 18:17:04 -0300

    Adicionado componentes de cabeçalho e busca

---
+++ src/components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container">
+    <h1>Relly</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Vue from 'vue';
 import App from './App.vue';
 
-
 Vue.config.productionTip = false;
 
 new Vue({
